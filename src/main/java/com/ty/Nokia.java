613c109360044c151ab181d2c2c5150eebe36706
truptilingaraju <ltrupti355@gmail.com>
2023-12-20 12:14:01 +0530

    first commit

---
+++ src/main/java/com/ty/Nokia.java
@@ -0,0 +1,14 @@
+package com.ty;
+
+import org.springframework.context.annotation.Primary;
+
+import org.springframework.stereotype.Component;
+
+@Component
+//@Primary
+public class Nokia implements Mobile {
+
+	public void text() {
+		System.out.println("nokia is texting");
+	}
+}
